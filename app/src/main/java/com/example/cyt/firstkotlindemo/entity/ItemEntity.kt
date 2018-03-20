261cc4c318e06908c73fdaffe416643aca8614da
cytcool <675149948@qq.com>
2018-03-20 23:25:07 +0800

    init TabLayout and ViewPager data

---
+++ app/src/main/java/com/example/cyt/firstkotlindemo/entity/ItemEntity.kt
@@ -0,0 +1,14 @@
+package com.example.cyt.firstkotlindemo.entity
+
+/**
+ * Created by CYT on 2018/3/20.
+ */
+class ItemEntity (val title:String,
+                  val describe:String,
+                  val image:String,
+                  val url:String,
+                  val time_and_class:String,
+                  val is_like:Boolean){
+
+
+}
--- app/src/main/java/com/example/cyt/firstkotlindemo/fragment/MyFragment.kt
+++ app/src/main/java/com/example/cyt/firstkotlindemo/fragment/MyFragment.kt
@@ -8,6 +8,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cyt.firstkotlindemo.entity.ItemEntity
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
+import org.jsoup.Jsoup
+
 
 /**
  * Created by CYT on 2018/3/20.
@@ -15,6 +21,7 @@ import android.view.ViewGroup
 class MyFragment: Fragment() {
 
     private var mRecycleView:RecyclerView?=null
+    private var mList= mutableListOf<ItemEntity>()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -31,6 +38,25 @@ class MyFragment: Fragment() {
     }
 
     private fun initRecyclerView(url:String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        async(UI){
+            val result=bg{
+                val jsoup=Jsoup.connect(url).get()
+                val uls=jsoup.select("ul.cf")
+
+                for(x in uls){
+                    val lis=x.select("li")
+                    for (i in lis){
+                        val title=i.select("div.lesson-infor > h2 > a").text()
+                        val item_url=i.select("div.lesson-infor > h2 > a").attr("href")
+                        val describe=i.select("div.lesson-infor > p").text()
+                        val image=i.select("div.lessonimg-box > a > img").attr("src")
+                        val time_and_class=i.select("div.lesson-infor > div > div:nth-child(l) > dl >dd.mar-b8>em").text()
+
+                        val entity=ItemEntity(title = title,url = item_url,describe = describe,image = image,time_and_class = time_and_class,is_like = false)
+                        mList.add(entity)
+                    }
+                }
+            }
+        }
     }
 }
