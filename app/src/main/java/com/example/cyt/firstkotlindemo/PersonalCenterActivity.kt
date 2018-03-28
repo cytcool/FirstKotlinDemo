56f2fae2b215bc45348ed4a016ee55bab67c5295
cytcool <675149948@qq.com>
2018-03-28 19:50:57 +0800

    init PersonalCenterActivity

--- app/src/main/java/com/example/cyt/firstkotlindemo/PersonalCenterActivity.kt
+++ app/src/main/java/com/example/cyt/firstkotlindemo/PersonalCenterActivity.kt
@@ -7,6 +7,7 @@ class PersonalCenterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_personal_center)
     }
 }
