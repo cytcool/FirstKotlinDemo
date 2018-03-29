4585ca43c8e976c7b7ccd0d793c3dcb8ee9f64bc
cytcool <675149948@qq.com>
2018-03-29 23:00:38 +0800

    init bottom_layout

--- app/src/main/java/com/example/cyt/firstkotlindemo/PersonalCenterActivity.kt
+++ app/src/main/java/com/example/cyt/firstkotlindemo/PersonalCenterActivity.kt
@@ -1,7 +1,9 @@
 package com.example.cyt.firstkotlindemo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_personal_center.*
 
 class PersonalCenterActivity : AppCompatActivity() {
 
@@ -9,5 +11,8 @@ class PersonalCenterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide()
         setContentView(R.layout.activity_personal_center)
+        setting.setOnClickListener {
+            startActivity(Intent(PersonalCenterActivity@this, SettingActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/cyt/firstkotlindemo/SettingActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cyt.firstkotlindemo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cyt.firstkotlindemo.R
+
+class SettingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
